64bab8bf73832d0452cb9d1c977e95c791549720
NembotJules <nembotjulesgithub@gmail.com>
2025-03-01 19:29:11 +0100

    Created the AlexNet architecture...

---
+++ AlexNet/model.py
@@ -0,0 +1,40 @@
+from torch.nn import nn
+
+class AlexNet(nn.Module): 
+    def __init__(self): 
+        super().__init__()
+        self.feature_extraction = nn.Sequential(
+            nn.Conv2d(3, 96, 11, stride = 4),
+            nn.ReLU(),
+            nn.MaxPool2d(3, stride = 2),
+
+            nn.Conv2d(96, 256, 5, stride = 1, padding = 2),
+            nn.ReLU(), 
+            nn.MaxPool2d(3, stride = 2),
+
+            nn.Conv2d(256, 384, 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(384, 384, 3, stride = 1, padding = 1),
+            nn.ReLU(),
+
+            nn.Conv2d(384, 256, 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(3, stride = 2)
+        )
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(9216, 4096)
+        self.dropout = nn.Dropout(p = 0.5)
+        self.fc2 = nn.Linear(4096, 200)
+
+
+    def forward(self, x): 
+        x = self.feature_extraction(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = nn.ReLU()(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = nn.ReLU()(x)
+        x = self.dropout(x)
+        x = nn.Softmax(dim = 1)(x)
+        return x 
