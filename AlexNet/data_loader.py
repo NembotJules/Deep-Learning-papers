2c83f2668e90e1147088e564a3d6abb166bd7939
NembotJules <nembotjulesgithub@gmail.com>
2025-03-02 20:21:46 +0100

    Added train_transform and val_transform...

--- AlexNet/data_loader.py
+++ AlexNet/data_loader.py
@@ -11,14 +11,25 @@ def get_dataloaders():
     """
     Load and return train and test dataloaders...
     """
-    transform = transforms.Compose([
-        transforms.Resize(Config.INPUT_SIZE), 
-        transforms.ToTensor(), 
+    # Training transforms (with augmentation for 227x227)
+    train_transform = transforms.Compose([
+    transforms.Resize(256),  
+    transforms.RandomCrop(227),  
+    transforms.RandomHorizontalFlip(p=0.5),  # 50% chance of flipping
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),  # Lighting changes
+    transforms.ToTensor(),  # Convert to tensor
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) 
+])
 
-    ])
-
-    train_dataset = datasets.ImageFolder(root='tiny-imagenet-200/train', transform = transform)
-    valid_dataset = datasets.ImageFolder(root = 'tiny-imagenet-200/val', transform= transform)
+   # Validation transforms (no augmentation)
+    val_transform = transforms.Compose([
+    transforms.Resize(256),  
+    transforms.CenterCrop(227),  
+    transforms.ToTensor(),  
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) 
+])
+    train_dataset = datasets.ImageFolder(root='tiny-imagenet-200/train', transform = train_transform)
+    valid_dataset = datasets.ImageFolder(root = 'tiny-imagenet-200/val', transform= val_transform)
 
     train_dataloader = DataLoader(train_dataset, batch_size = Config.BATCH_SIZE, num_workers=Config.NUM_WORKERS, shuffle = True)
     val_dataloader = DataLoader(valid_dataset, batch_size = Config.BATCH_SIZE, num_workers=Config.NUM_WORKERS, shuffle = True)
